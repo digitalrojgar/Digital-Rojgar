42c5866ef0bee06711ed426a56582f002afe3c7b
Deepak singh Baghel <deepaktechdigest@gmail.com>
2025-08-19 14:39:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -57,3 +57,9 @@ function toggleMenu() {
   document.getElementById("nav-links").classList.toggle("active");
 }
 </script>
+const menuToggle = document.querySelector('.menu-toggle');
+const navLinks = document.querySelector('.nav-links');
+
+menuToggle.addEventListener('click', () => {
+  navLinks.classList.toggle('active');
+});
